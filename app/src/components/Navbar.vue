1b3b01760c6773933c08e07d8c14352fdfcf6d23
krushndayshmookh <krushndayshmookh@gmail.com>
2019-12-26 10:17:51 +0530

    test

---
+++ app/src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .navbar-fixed
+    nav
+      .nav-wrapper
+
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,18 +1,18 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+  .home
+    Navbar
+    img(alt="Vue logo" src="../assets/logo.png" )
+  
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Navbar from "@/components/Navbar.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Navbar
   }
 };
 </script>
