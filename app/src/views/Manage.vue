5d8af5d11f8112461d438666c403ed0448fdf2ec
krushndayshmookh <krushndayshmookh@gmail.com>
2020-01-21 02:23:05 +0530

    create manage

---
+++ app/src/views/Manage.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  .home
+    Navbar
+    section.section
+      .container
+        
+        h4.light Cert Title
+        router-link.btn.blue(to="/dashboard/new") Edit Cert
+
+    section.section
+      .container
+        h4.light Participants
+        .collection
+          a.collection-item cert 1
+              
+          a.collection-item cert 2
+
+          a.collection-item cert 3
+  
+
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+import M from 'materialize-css'
+
+export default {
+  name: 'home',
+  components: {
+    Navbar
+  },
+  mounted() {
+    M.AutoInit()
+  }
+}
+</script>
