78e1d6facbce48dbcb8daed92b021eaf109d7016
krushndayshmookh <krushndayshmookh@gmail.com>
2020-01-21 02:04:36 +0530

    create home

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,18 +1,29 @@
 <template lang="pug">
   .home
     Navbar
-    img(alt="Vue logo" src="../assets/logo.png" )
+    section.section
+      .container
+        .parallax-container.valign-wrapper
+          .parallax
+            img(src="../assets/images/office.jpg")
+          .container.center
+            .btn-large.center Get It
+      
   
 </template>
 
 <script>
 // @ is an alias to /src
-import Navbar from "@/components/Navbar.vue";
+import Navbar from '@/components/Navbar.vue'
+import M from 'materialize-css'
 
 export default {
-  name: "home",
+  name: 'home',
   components: {
     Navbar
+  },
+  mounted() {
+    M.AutoInit()
   }
-};
+}
 </script>
