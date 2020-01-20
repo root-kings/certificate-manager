23580d385803e6c2d689941e3d640e32273826f9
krushndayshmookh <krushndayshmookh@gmail.com>
2020-01-21 02:03:56 +0530

    create dashbord

---
+++ app/src/views/Dashboard.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  .home
+    Navbar
+    section.section
+      .container
+        .btn.blue New Cert
+
+    section.section
+        .container
+            .collection
+                a.collection-item cert 1
+                    
+                a.collection-item cert 2
+
+                a.collection-item cert 3
+      
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+import M from 'materialize-css'
+
+export default {
+  name: 'home',
+  components: {
+    Navbar
+  },
+  mounted() {
+    M.AutoInit()
+  }
+}
+</script>
