ae55861d3396db366a515e235a425a4c9b5030b7
krushndayshmookh <krushndayshmookh@gmail.com>
2020-01-21 02:03:06 +0530

    create nav

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -1,17 +1,25 @@
 <template lang="pug">
   .navbar-fixed
-    nav
+    nav.transparent.z-depth-0
       .nav-wrapper
+        .container
+          a.brand-logo.blue-text(href="") Certificate Manager
+          ul#nav-mobile.right.hide-on-med-and-down
+            li
+              a.blue-text(href="sass.html") Sass
+            li
+              a.blue-text(href="badges.html") Components
+            li
+              a.blue-text(href="collapsible.html") JavaScript
+
 
 </template>
 
 <script>
 export default {
-  name: "Navbar",
+  name: "Navbar"
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style scoped></style>
