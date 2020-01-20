4678133545c2e19b0753dea66d902a68bfce38cd
krushndayshmookh <krushndayshmookh@gmail.com>
2020-01-21 02:03:42 +0530

    create routes for dashboard and new cert

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -18,6 +18,18 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/dashboard/new",
+    name: "new",
+    component: () =>
+      import( "../views/New.vue")
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: () =>
+      import( "../views/Dashboard.vue")
   }
 ];
 
