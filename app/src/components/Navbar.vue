54b9f2ef3ddeaf6183eee976003bee18b96cc2bf
krushndayshmookh <krushndayshmookh@gmail.com>
2020-01-21 14:28:45 +0530

    apply style to nav

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -3,14 +3,15 @@
     nav.white.z-depth-0
       .nav-wrapper
         .container
-          router-link.brand-logo.deep-purple-text.text-darken-3(to="/") Certificate Manager
+          router-link.brand-logo.deep-purple-text.text-darken-3.light(to="/") Certificate Manager
           ul#nav-mobile.right.hide-on-med-and-down
             li
-              router-link.deep-purple-text.text-darken-3(to="/dashboard") Dashboard
+              router-link.black-text.light(to="/dashboard") Dashboard
             li
-              router-link.deep-purple-text.text-darken-3(to="/about") About
+              router-link.black-text.light(to="/about") About
             li
-              router-link.deep-purple-text.text-darken-3(to="login") Login
+              router-link.black-text.light(to="login") Login
+      .divider
 
 
 </template>
@@ -22,4 +23,27 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style lang="scss" scoped>
+@import 'materialize-css/sass/materialize.scss';
+
+@media only screen and (min-width: 601px) {
+  nav,
+  nav .nav-wrapper i,
+  nav a.sidenav-trigger,
+  nav a.sidenav-trigger i {
+    height: 48px;
+    line-height: 48px;
+  }
+}
+nav {
+  color: #fff;
+  background-color: #ee6e73;
+  width: 100%;
+  height: 48px;
+  line-height: 48px;
+}
+
+.router-link-active {
+  color: color('deep-purple', 'darken-1') !important;
+}
+</style>
