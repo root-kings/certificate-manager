54f5f807303ae35359e61fe7a4fad92bfcabde21
krushndayshmookh <krushndayshmookh@gmail.com>
2020-01-21 12:10:40 +0530

    create footer

---
+++ app/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  footer.page-footer.white
+    .divider
+    .container
+      .row
+        .col.l6.s12
+          h5.deep-purple-text.text-darken-1 Footer Content
+          p.deep-purple-text
+            | You can use rows and columns here to organize your footer content.
+        .col.l4.offset-l2.s12
+          h5.deep-purple-text.text-darken-1 Links
+          ul
+            li
+              a.deep-purple-text(href="#!") Link 1
+            li
+              a.deep-purple-text(href="#!") Link 2
+            li
+              a.deep-purple-text(href="#!") Link 3
+            li
+              a.deep-purple-text(href="#!") Link 4
+    .footer-copyright
+      .container.deep-purple-text
+        | © 2014 Copyright Text
+        a.deep-purple-text.right(href="#!") More Links
+
+</template>
