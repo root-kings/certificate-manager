4e51b1239e5b9927227248646356eb17eece4b8b
krushndayshmookh <krushndayshmookh@gmail.com>
2020-01-21 12:10:55 +0530

    create Layout

---
+++ app/src/components/Layout.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  .layout
+    header
+      Navbar
+    main
+      slot
+    Footer
+</template>
+
+<script>
+// @ is an alias to /src
+import M from 'materialize-css'
+
+import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Navbar,
+    Footer
+  },
+  mounted() {
+    M.AutoInit()
+  }
+}
+</script>
