3f216eab56db765996cdef6346727ddc6fa9abd0
krushndayshmookh <krushndayshmookh@gmail.com>
2020-01-21 12:14:06 +0530

    beautify

--- app/src/main.js
+++ app/src/main.js
@@ -1,17 +1,17 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
 
-import "materialize-css";
-import "materialize-css/dist/css/materialize.css";
-import "./assets/css/lib/dayshmookh.layout.css";
+import 'materialize-css'
+import 'materialize-css/dist/css/materialize.css'
+import './assets/css/lib/dayshmookh.layout.css'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
