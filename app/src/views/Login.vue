454050393013934c12ff18bc7f67e7e2319869be
krushndayshmookh <krushndayshmookh@gmail.com>
2020-01-21 12:13:35 +0530

    create login

---
+++ app/src/views/Login.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  Layout
+    .login
+      section.section
+          .container
+              .row
+                .col.s12.m4.offset-m4
+                  .card-panel      
+  
+</template>
+
+<script>
+import Layout from '@/components/Layout.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Layout
+  }
+}
+</script>
