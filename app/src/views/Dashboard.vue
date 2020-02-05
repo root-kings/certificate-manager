4339d87964a6019c6a6feb7a8fcffd3a41a65ed8
krushndayshmookh <krushndayshmookh@gmail.com>
2020-02-05 13:36:09 +0530

    fix sample routes

--- app/src/views/Dashboard.vue
+++ app/src/views/Dashboard.vue
@@ -3,16 +3,16 @@
     .dashboard
       section.section
         .container
-          router-link.btn.deep-purple.darken-3(to="/dashboard/new") New Cert
+          router-link.btn.deep-purple.darken-3(to="/dashboard/certificate/new") New Cert
 
       section.section
           .container
               .collection
-                  router-link.collection-item(to="/dashboard/cert") cert 1
+                  router-link.collection-item(to="/dashboard/certificate/1") cert 1
                       
-                  router-link.collection-item(to="/dashboard/cert") cert 2
+                  router-link.collection-item(to="/dashboard/certificate/2") cert 2
 
-                  router-link.collection-item(to="/dashboard/cert") cert 3
+                  router-link.collection-item(to="/dashboard/certificate/3") cert 3
         
   
 </template>
