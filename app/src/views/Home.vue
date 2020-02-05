ec74535ba685f025b2edd2d92c264003c05b06e6
krushndayshmookh <krushndayshmookh@gmail.com>
2020-02-05 13:36:17 +0530

    design home

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -2,12 +2,20 @@
   Layout
     .home
       section.section
-        .container
+        .container.rounded.z-depth-5
           .parallax-container.valign-wrapper
             .parallax
-              img(src="../assets/images/office.jpg")
+              img.dark(src="../assets/images/office.jpg")
             .container.center
+              h1.light.center.white-text Certificate Manager
+              p.flow-text.center.white-text Generate, manage and distribute digitally signed verifiable certificates for all events at low costs.
+      
               router-link.btn-large.center.deep-purple.darken-3(to="/dashboard") Get It
+      section.section
+        .container.center
+          h3.light.center Download your certificates
+          router-link.btn-large.center.deep-purple.darken-3.z-depth-5(to="/certificates") Download
+
       
   
 </template>
