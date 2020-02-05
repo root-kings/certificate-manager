a231be0966e7e9ed3c03d5820535bfa224c32b27
krushndayshmookh <krushndayshmookh@gmail.com>
2020-02-05 14:19:35 +0530

    populate dashboard

--- app/src/views/Dashboard.vue
+++ app/src/views/Dashboard.vue
@@ -3,27 +3,37 @@
     .dashboard
       section.section
         .container
-          router-link.btn.deep-purple.darken-3(to="/dashboard/certificate/new") New Cert
+          router-link.btn.deep-purple.darken-3(to="/dashboard/certificate/new") New Certificate
 
       section.section
-          .container
-              .collection
-                  router-link.collection-item(to="/dashboard/certificate/1") cert 1
-                      
-                  router-link.collection-item(to="/dashboard/certificate/2") cert 2
-
-                  router-link.collection-item(to="/dashboard/certificate/3") cert 3
-        
-  
+        .container
+          .collection
+            router-link.collection-item(v-for="certificate in certificates" :key="certificate._id" :to="'/dashboard/certificate/'+certificate._id") {{ certificate.title }} - {{ certificate.date }}
+                
 </template>
 
 <script>
 import Layout from '@/components/Layout.vue'
+import axios from 'axios'
 
 export default {
   name: 'home',
   components: {
     Layout
+  },
+  data() {
+    return { certificates: [] }
+  },
+  mounted() {
+    this.fetchCertificates()
+  },
+
+  methods: {
+    fetchCertificates() {
+      axios
+        .get(process.env.VUE_APP_API_ENDPOINT + '/certificate/dashboard')
+        .then(response => (this.certificates = response.data))
+    }
   }
 }
 </script>
